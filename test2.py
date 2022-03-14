265469c77f14a5449ba7cde9b5660dedc119de1a
Inhan <inhan50712312@gmail.com>
2022-03-15 01:12:01 +0900

    commit test

--- test2.py
+++ test2.py
@@ -8,3 +8,14 @@ for i in b :
         cnt += 1
 print(cnt)
 
+n , m = map(int, input().split())
+cards = list(map(int,input().split()))
+
+target = 0
+for i in range(len(cards)):
+    for j in range(i+1,len(cards)):
+        for k in range(j+1,len(cards)):
+            if target <= cards[i]+cards[j]+cards[k] <= m :
+                target = cards[i]+cards[j]+cards[k]
+print(target)
+
