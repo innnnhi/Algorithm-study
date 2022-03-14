d2ac5c7fd9bb4cb5d90bc06c4912d8c3e784625e
Inhan <inhan50712312@gmail.com>
2022-03-15 01:58:20 +0900

    add Details

--- sort_prob/Q1181.py
+++ sort_prob/Q1181.py
@@ -1,8 +1,13 @@
+# Q1181
+# 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+# 길이가 짧은 것부터
+# 길이가 같으면 사전 순으로
+
 n = int(input())
 
-s = [input() for _ in range(n)]
-t = list(set(s))
-t.sort(key= lambda x: (len(x), x))
-for j in t:
+words = [input() for _ in range(n)]
+delete_same = list(set(words)) # 중복단어 삭제
+delete_same.sort(key= lambda x: (len(x), x)) # 람다 사용 1 : 길이별로 먼저 정렬 2 : 같은길이의 단어 올 시 사전순으로
+for j in delete_same:
     print(j)
 
--- sort_prob/Q2751.py
+++ sort_prob/Q2751.py
@@ -1,9 +1,11 @@
 import sys
+# Q1181 : N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+# 입력받아 오는 것이 메인인 문제
 
 n = int(input())
 arr = []
 for i in range(n):
-    arr.append(int(sys.stdin.readline().strip()))
+    arr.append(int(sys.stdin.readline().strip())) # strip => 한 줄로 세운다 즉 세로로 입력을 받고싶을 때 사용
 arr.sort()
 for j in arr:
     print(j)
